b6d19837f095b52065a682e83957a93b38333e28
조경은 <kavvy99@naver.com>
2023-05-27 20:28:42 +0900

    #5 feat Adjust input location
    to be Left Alignment, refactor '.infos' to '.info'

--- js/home.js
+++ js/home.js
@@ -1,15 +1,27 @@
 // #btnStart click -> #join 회원가입 openJoin
 const startBtn = document.querySelector('#btnStart');
 const join = document.querySelector('#join');
+const joinUsername = document.querySelector('#username');
 const HIDDEN_CLASSNAME = 'hidden';
 
-function openJoin(){
+
+function openJoin(event){
+    event.preventDefault();
     join.classList.remove(HIDDEN_CLASSNAME);
     startBtn.classList.add(HIDDEN_CLASSNAME);
     goLogin.classList.add(HIDDEN_CLASSNAME);
     login.classList.add(HIDDEN_CLASSNAME);
+    const typedUsername = joinUsername.value;
+    localStorage.setItem(USERNAME_KEY, typedUsername);
+    paintMainTitle(typedUsername);
 }
-startBtn.addEventListener('click',openJoin);
+startBtn.addEventListener('click', openJoin);
+
+const mainTitle = document.querySelector('#mainTitle');
+
+function paintMainTitle(username) {
+    mainTitle.innerText = `${username}님의 담벼락`;
+}
 
 // #goLogin click -> #login 로그인 openLogin
 const goLogin = document.querySelector('#goLogin');
