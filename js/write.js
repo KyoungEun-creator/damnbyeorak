8bce416f2a2d3a7527bc1f5258bb737a87befb9e
조경은 <kavvy99@naver.com>
2023-07-06 17:36:03 +0900

    #20 refactor: Reorganize codes in write.html
    considering events on write.js

--- js/write.js
+++ js/write.js
@@ -1,16 +1,22 @@
-// #btnWrite click -> #join 회원가입 openJoin
-const writeBtn = document.querySelector('#btnWrite');
+// default: (#mark1_on, #mark2_off, #mark3_off) shown
+// default: (#mark1_off, #mark2_on, #mark3_on) hidden
 
-const HIDDEN_CLASSNAME = 'hidden';
+// #mark2_off click -> #mark2_on 의 hidden 클래스 없어짐
+//                  -> #mark2_off 에 hidden 클래스 생김
+//                  -> #mark1_off 에 hidden 클래스 없어짐
+//                  -> #mark1_on 에 hidden 클래스 생김
 
 
-function openLetter(){
-    join.classList.remove(HIDDEN_CLASSNAME);
-    startBtn.classList.add(HIDDEN_CLASSNAME);
-    goLogin.classList.add(HIDDEN_CLASSNAME);
-    login.classList.add(HIDDEN_CLASSNAME);
-    const typedUsername = joinUsername.value;
-    localStorage.setItem(USERNAME_KEY, typedUsername);
-    paintMainTitle(typedUsername);
-}
-writeBtn.addEventListener('click', openLetter);
+// #mark3_off click -> #mark3_on 의 hidden 클래스 없어짐
+//                  -> #mark3_off 에 hidden 클래스 생김
+//                  -> #mark2_off 에 hidden 클래스 없어짐
+//                  -> #mark2_on 에 hidden 클래스 생김
+
+// 결론
+// 1. #markX_off click 하면 
+// 2. #markX_off 안 보여야지: hidden 클래스 add
+// 3. #markX_on 보여야지: hidden 클래스 remove
+
+// 4. 나머지 두 마크스타일의 _on 은 안 보여야지: hidden 클래스 add
+// 5. 나머지 두 마크스타일의 _off 는 보여야지: hidden 클래스 remove
+
